00a1f58961e5eea15579cc5402ebfa3e0844b1b4
hearts <hearts.zhang@gmail.com>
2015-06-23 17:12:37 +0800

    - remove bower
    - cleanup code
    - use node_modules

--- js/default.js
+++ js/default.js
@@ -17,36 +17,6 @@
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
     "use strict";
-  // track whether the item was bought or not
-    var bought;
-
-  //Show the flyout
-    function showConfirmFlyout() {
-      bought = false;
-      log("");
-
-      var buyButton = document.getElementById("buyButton");
-      document.getElementById("confirmFlyout").winControl.show(buyButton);
-    }
-
-  // When the Buy button is pressed, hide the flyout since the user is done with it.
-    function confirmOrder() {
-      bought = true;
-      log("You have completed your purchase.");
-      document.getElementById("confirmFlyout").winControl.hide();
-    }
-
-  // On dismiss of the flyout, determine if it closed because the user pressed the buy button. If not, then the
-  // flyout was light dismissed.
-    function onDismiss() {
-      if (!bought) {
-        log("The purchase was not completed.");
-      }
-    }
-
-    function log(message) {
-      document.getElementById("status").textContent = message;
-    }
 
     var app = WinJS.Application;
     if (WinJS.Utilities.hasWinRT) {
@@ -79,40 +49,15 @@
       app.onready = function () {
       };
     }
-    function basicFragmentLoad() {
-      resetOutput();
 
-      // Read fragment from the HTML file and load it into the div.  Note
-      // Fragments.renderCopy() returns a promise which a done() statement
-      // is attached to in order to perform additional processing or handle errors
-      // that may have occurred during the renderCopy() action.
-      WinJS.UI.Fragments.renderCopy("/page-toolbar.html", basicFragmentLoadDiv)
-          .done(
-              function () {
-                log("successfully loaded fragment", "sample", "status");
-              },
-              function (error) {
-                log("error loading fragment: " + error, "sample", "error");
-              }
-          );
-    }
-    function resetOutput() {
-      basicFragmentLoadDiv.innerHTML = "";
-    }
-
-    function log(msg) {
-    }
     app.addEventListener("ready", function () {
       WinJS.UI.processAll().done(function () {
-        var splitView = document.querySelector(".splitView").winControl;
-        new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
+//        var splitView = document.querySelector(".splitView").winControl;
+//        new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
       });
-      console.log("you should shee this");
-      var basicFragmentLoadDiv = document.querySelector("#basicFragmentLoadDiv");
-      WinJS.Utilities.query("#basicFragmentLoadButton", document).listen("click", basicFragmentLoad);
 
       var renderHost = document.querySelector(".renderingPageControls-renderedControl");
-      WinJS.UI.Pages.render("/toolbar.html", renderHost).done(function () {
+      WinJS.UI.Pages.render("/pages/ctrl/listview.html", renderHost).done(function () {
       });
     }, false);
     app.start();
