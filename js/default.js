84ea5a7a233cf047887a2cf4ff433b99e92d616a
hearts <hearts.zhang@gmail.com>
2015-06-23 10:48:48 +0800

    - init

---
+++ js/default.js
@@ -0,0 +1,42 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    if (WinJS.Utilities.hasWinRT) {
+      var activation = Windows.ApplicationModel.Activation;
+      app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+          if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+            // TODO: This application has been newly launched. Initialize
+            // your application here.
+          } else {
+            // TODO: This application has been reactivated from suspension.
+            // Restore application state here.
+          }
+          //            args.setPromise(WinJS.UI.processAll());
+          WinJS.UI.processAll().done(function () {
+            var splitView = document.querySelector(".splitView").winControl;
+            new WinJS.UI._WinKeyboard(splitView.paneElement); // Temporary workaround: Draw keyboard focus visuals on NavBarCommands
+          });
+        }
+      };
+
+      app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+      };
+    } else {
+      console.log("you should shee here");
+      app.onready = function () {
+        WinJS.UI.processAll();
+      };
+    }
+
+    app.start();
+})();
